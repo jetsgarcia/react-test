d679e8bba02676f25457da547ded221db2d2d487
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2024-01-05 20:02:36 +0800

    Recreated the whole UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import TaskInput from "./components/task_input/task_input";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <TaskInput />
+      <div className="task-input-container">
+        <form action="">
+          <input type="text" id="item" />
+          <button>+</button>
+        </form>
+      </div>
+      <ul className="all-tasks">
+        <li>
+          <input type="checkbox" id="" />
+          <p>Task</p>
+        </li>
+        <li>
+          <input type="checkbox" id="" />
+          <p>Task</p>
+        </li>
+        <li>
+          <input type="checkbox" id="" />
+          <p>Task</p>
+        </li>
+      </ul>
     </>
   );
 }
