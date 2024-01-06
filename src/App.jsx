a1c06f74da01d9d5dbdaeff419fdf0a33a25199d
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2024-01-06 14:15:37 +0800

    Added the logic for inputting todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { id: crypto.randomUUID(), description: newItem, completed: false },
+    ]);
+    setTodos([
+      ...todos,
+      { id: crypto.randomUUID(), description: newItem, completed: false },
+    ]);
+    setNewItem("");
+  }
+
+  console.log(todos);
   return (
     <>
       <div className="task-input-container">
-        <form action="">
-          <input type="text" id="item" />
-          <button>+</button>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+          <button type="submit">+</button>
         </form>
       </div>
       <ul className="all-tasks">
-        <li>
-          <input type="checkbox" id="" />
-          <p>Task</p>
-        </li>
-        <li>
-          <input type="checkbox" id="" />
-          <p>Task</p>
-        </li>
-        <li>
-          <input type="checkbox" id="" />
-          <p>Task</p>
-        </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" id="" />
+            <p>{todo.description}</p>
+          </li>
+        ))}
       </ul>
     </>
   );
