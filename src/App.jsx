88ecde1483deec854faa58e3339039da156fed8e
Jetter Garcia <102631417+jetsgarcia@users.noreply.github.com>
2024-01-08 18:34:07 +0800

    Added the logic for toggling a task

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,17 @@ function App() {
     setNewItem("");
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: completed };
+        }
+        return todo;
+      })
+    );
+  }
+
   return (
     <>
       <div className="task-input-container">
@@ -29,11 +40,17 @@ function App() {
       </div>
       <ul className="all-tasks">
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} className={todo.completed ? "completed" : ""}>
             <label>
-              <input type="checkbox" id="" value={todo.completed} />
+              <input
+                type="checkbox"
+                id=""
+                value={todo.completed}
+                onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+              />
               {todo.description}
             </label>
+            <button>Delete</button>
           </li>
         ))}
       </ul>
